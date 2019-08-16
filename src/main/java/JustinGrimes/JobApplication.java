1f8f8eded66cbc92a5b5cb8a9064c4136525ebfb
Justin Grimes <jusgrimes@gmail.com>
2019-08-15 21:08:20 -0500

    added add test for DAO

--- src/main/java/JustinGrimes/JobApplication.java
+++ src/main/java/JustinGrimes/JobApplication.java
@@ -1,6 +1,7 @@
 package JustinGrimes;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class JobApplication {
     private int id;
@@ -38,5 +39,21 @@ public class JobApplication {
 
     public String getNotes() {
         return notes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobApplication that = (JobApplication) o;
+        return companyName.equals(that.companyName) &&
+                applicationDate.equals(that.applicationDate) &&
+                status == that.status &&
+                notes.equals(that.notes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(companyName, applicationDate, status, notes);
     }
 }
