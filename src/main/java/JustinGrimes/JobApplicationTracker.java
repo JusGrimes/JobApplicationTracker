6ba3a190f83e88e8675a96725225b0f9a54950b6
Justin Grimes <jusgrimes@gmail.com>
2019-08-18 17:58:38 -0500

    removed JobApplicationTracker to be a JFrame

--- src/main/java/JustinGrimes/JobApplicationTracker.java
+++ src/main/java/JustinGrimes/JobApplicationTracker.java
@@ -1,37 +1,54 @@
 package JustinGrimes;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
-import java.sql.Connection;
 import java.sql.SQLException;
 
+@SuppressWarnings("WeakerAccess")
 public class JobApplicationTracker extends JFrame {
-    private final static String appName = "Job Application Tracker";
-
-    private Connection conn;
-    private MainWindow mainWindow;
-    private Dao<JobApplication> jobAppDao;
-
-    public JobApplicationTracker() throws HeadlessException {
-        super(appName);
-        try {
-            this.setMinimumSize(new Dimension(1000, 500));
-            initComponents();
-            jobAppDao = new JobApplicationDao();
-        }
-        catch(SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-
-    private void initComponents() {
-        mainWindow = new MainWindow(this);
-    }
+    private JPanel rootPanel;
+    private JTable applicationTable;
+    private JButton addButton;
+    private JButton deleteButton;
+    private JButton editButton;
+    private JScrollPane scrollPane;
+    private JPanel headerPanel;
+    private JPanel buttonPanel;
+    private Dao<JobApplication> applicationDao;
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(() ->
-            new JobApplicationTracker().setVisible(true)
+        SwingUtilities.invokeLater(()->{
+                    new JobApplicationTracker();
+                }
         );
+    }
+
+    public JobApplicationTracker() {
+        super("Job Application Tracker");
+        this.setContentPane(this.rootPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        AppTableModel appTableModel = null;
+        try {
+            applicationDao = new JobApplicationDao();
+            appTableModel = new AppTableModel(applicationDao.getAll());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        applicationTable = new JTable(appTableModel);
+        applicationTable.getTableHeader().setReorderingAllowed(false);
+
+        // set all of the cells to centered
+        DefaultTableCellRenderer cellRenderer = new DefaultTableCellRenderer();
+        cellRenderer.setHorizontalAlignment(JLabel.CENTER);
+        for (int i = 0; i < applicationTable.getColumnCount(); i++) {
+            applicationTable.getColumnModel().getColumn(i).setCellRenderer(cellRenderer);
+        }
     }
 }
